94c954ffea5a5144a709aff94bce2109e8a6f0a1
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2021-07-21 18:23:08 -0700

    first commit

---
+++ callApply.js
@@ -0,0 +1,8 @@
+const normalPerson = {
+    firstName: 'Rahim',
+    lastName: 'Khan',
+    getFullName: function(){
+        console.log(this.firstName, this.lastName);
+    }
+}
+console.log(normalPersson.firstName);
